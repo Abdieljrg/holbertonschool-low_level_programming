b24ac5d4b517a7309f1926f50e385a64ce3d4182
abdieljrg <abdieljrg@gmail.com>
2024-02-23 10:26:01 -0400

    2 finally

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - alphabetic loop
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+	{
+		putchar(abc);
+
+	}
+		putchar('\n');
+
+return (0);
+
+}
