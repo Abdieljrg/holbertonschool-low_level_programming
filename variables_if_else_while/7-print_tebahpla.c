5a1b3b76e9fdf36563df548ec1dcd366f47149e2
abdieljrg <abdieljrg@gmail.com>
2024-02-23 11:15:33 -0400

    7 new

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - alphabetic loop
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'z'; abc >= 'a'; abc--)
+
+	{
+		putchar(abc);
+
+	}
+		putchar('\n');
+
+return (0);
+
+}
