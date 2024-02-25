6143a4727818b80e80b4a0453986dc8439f9e78b
abdieljrg <abdieljrg@gmail.com>
2024-02-24 23:06:24 -0400

    3 1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_islower -check lower case
+ *
+ *Return: 1 for lowercase and 0 for anything that is not lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && C <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
