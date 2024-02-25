b8840611b30d40811da85f1d1cf3c3936e2c1b2b
abdieljrg <abdieljrg@gmail.com>
2024-02-24 23:42:26 -0400

    4 1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha -check alpha char
+ *
+ * @c: the char to be check
+ *
+ * Return: 1 for alpha char or 0 else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+	{
+return (1);
+	}
+return (0);
+	}
