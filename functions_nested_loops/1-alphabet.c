6a59dc3849e0fc652ed340f20964a8d0ce9675e7
abdieljrg <abdieljrg@gmail.com>
2024-02-24 21:03:32 -0400

    1 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - putchar prints alphabet
+ *
+ *
+ * Return (0)
+ */
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+	{
+	_putchar(abc);
+	}
+	{
+	_putchar('\n');
+	}
+return (0);
+}
