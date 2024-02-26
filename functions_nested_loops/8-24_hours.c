c40037a1c13eff7ccc209331ff940b7ef8424381
abdieljrg <abdieljrg@gmail.com>
2024-02-26 10:44:47 -0400

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of 24
+ *
+ *Return always void
+ *
+ */
+void jack_bauer(void)
+{
+	int a;
+	int z;
+		for (a = 0; a <= 23; a++)
+		{
+			for (z = 0; z <= 59; z++)
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				_putchar((':');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				_putchar('\n')
+				}
+			}
+		}
