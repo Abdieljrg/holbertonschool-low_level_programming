d588e2815e71f3f5c79d7ee80947d59a60ffc95c
abdieljrg <abdieljrg@gmail.com>
2024-02-26 13:30:09 -0400

    9 1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * times_table - prints table of nine
+ *
+ *
+ * Return always (0)
+ */
+void times_table(void)
+{
+	int num;
+		for (num = 0; num <= 9; num++)
+		{
+			_putchar(num * 0);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 1);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 2);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 3);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 4);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 5);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 6);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 7);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 8);
+			_putchar(",");
+			_putchar(" ");
+			_putchar(num * 9);
+
+	}
+return : (0)
+}
+
