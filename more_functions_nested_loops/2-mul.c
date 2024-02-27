1f617e93e6898944ae5338aa25455b2219adfc2e
abdieljrg <abdieljrg@gmail.com>
2024-02-27 14:13:39 -0400

    kamehameha

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplication about two int var
+ *
+ * @a: first number to multiply
+ * @b: second number
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int pp;
+
+	pp = a * b;
+
+	return (pp);
+}
