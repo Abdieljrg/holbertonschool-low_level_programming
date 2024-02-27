6609fd6d67c986ecb45789d261d14d3cf72f01ca
abdieljrg <abdieljrg@gmail.com>
2024-02-27 13:26:52 -0400

    0 1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks lower case
+ *
+ * @c: char to be examinated
+ *
+ * Return: 1 if it is lower case, 0 if else
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && <= 'z')
+	{
+		return (1);
+		}
+	return (0);
+	}
