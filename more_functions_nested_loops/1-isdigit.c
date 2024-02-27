1d531fa1f7136fbf74bad14573199f43df4c7747
abdieljrg <abdieljrg@gmail.com>
2024-02-27 13:51:12 -0400

    1 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - checks lower case
+ *
+ * @c: int to be examinated
+ *
+ * Return: 1 if it is a digit , 0 if else
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+		}
+	return (0);
+	}
