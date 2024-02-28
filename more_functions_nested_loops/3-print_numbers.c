0825f3543bd2a1abf9bf5eb4040f90f078168cdb
abdieljrg <abdieljrg@gmail.com>
2024-02-28 10:17:55 -0400

    3 1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main"
+#include <stdio.h>
+/**
+ *print_numbers - print a set of numbers
+ *
+ * Return: always (0)
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar (num);
+	}
+	_putchar ('\n')
+	return (0);
+}
