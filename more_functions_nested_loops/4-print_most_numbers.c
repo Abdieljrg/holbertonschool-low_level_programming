24aa4ee10ebc10de08bf1349e93d7d0b1e6d89ab
abdieljrg <abdieljrg@gmail.com>
2024-02-28 13:40:10 -0400

    4 1

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_most_numbers - prints nmber except 2 and 4
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int x = 2;
+	int y = 4;
+	int num;
+
+for (num = '0'; num <= '9'; num++)
+	{
+	if (num != x && num != y)
+		{
+		_putchar(num);
+		}
+	{
+	_putchar('\n');
+	}
+	}
+}
