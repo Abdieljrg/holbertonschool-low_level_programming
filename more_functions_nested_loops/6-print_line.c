67fb367e1947a77ad476e07e67cfdb41734c9045
abdieljrg <abdieljrg@gmail.com>
2024-03-04 10:07:31 -0400

    gg 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -13,13 +13,13 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (num = 1; num <= n; num++)
-			{
-				_putchar('_');
-			}
-			_putchar('\n');
-		}
-		else
 		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
 	_putchar('\n');
-}
+	}
 }
