6986883af7f701e1fafa25546b4eb8d437e62e89
abdieljrg <abdieljrg@gmail.com>
2024-03-04 13:15:34 -0400

    gg 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - start of the program
+ *@n: pointer
+ *Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+n
+inters 
+and 
+arrays
