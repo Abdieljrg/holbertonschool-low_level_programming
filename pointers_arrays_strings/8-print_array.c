8618af05d2b8e25d4c9e5e40c8e17fdd8359b4ec
abdieljrg <abdieljrg@gmail.com>
2024-03-05 18:39:22 -0400

    gg ezzzz

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - prints the array
+ *@a: pointer in use
+ *@n:variable in use
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int goku;
+
+	for (goku = 0; goku < n; goku++)
+	{
+		printf("%d%s", a[goku], (goku == n - 1) ? "" : ", ");
+	}
+	_putchar('\n');
+}
