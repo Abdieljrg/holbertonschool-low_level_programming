0da5e4ca4618cf8d7e8eefcfadac2a3f829c90ca
abdieljrg <abdieljrg@gmail.com>
2024-03-05 17:48:54 -0400

    gg 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *puts2 - jumps char from 2 and 2
+ *@str: pointer in use
+ *Return: void
+ */
+void puts2(char *str)
+{
+	int a = 0;
+	int b = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	while (b < a)
+	{
+		_putchar(str[a]);
+
+		a += 2;
+	}
+	_putchar('\n');
+}
