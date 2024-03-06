0a86d15d305723a671c7503c7ae9f35bfd22228e
Abdieljrg <abdieljrg@gmail.com>
2024-03-06 16:34:49 -0400

    My Commit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strncat - beggining of program
+ *@dest: pointer
+ *@src: pointer two
+ *@n: variable in use
+ *Return: Dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = n;
+
+	return (dest);
+}
