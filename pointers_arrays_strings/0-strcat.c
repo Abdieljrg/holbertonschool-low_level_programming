76e2dcbd410c4c5050b177b3d2e03e5c9347848f
Abdieljrg <abdieljrg@gmail.com>
2024-03-06 16:02:52 -0400

    My Commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcat - concatonates two pointers
+ *@dest: pointer one
+ *@src: pointer two
+ *Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+	int y= 0;
+
+	while (dest[x] != '\0')
+	{
+		X++;
+	}
+
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		j++
+	}
+	dest[x] = '\0';
+
+	return dest;
+}
