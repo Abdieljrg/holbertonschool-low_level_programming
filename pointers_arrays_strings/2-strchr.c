01104790968f6a93e1b03ee8faec2e4d06666d17
Abdieljrg <abdieljrg@gmail.com>
2024-03-08 15:41:12 -0400

    My Commit

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (s != '\0')
+	while (*s != '\0')
 	{
-		if (s == c)
+		if (*s == c)
 		{
-			return(s);
+			return (s);
 		}
 		s++;
 	}
-	if (s == c)
+	if (*s == c)
 	{
 		return (s);
 	}
-	reutn (NULL);
+	return (NULL);
 }
