d78fdde0be4f21693e234fbc70aa72c4ea09d93d
Abdieljrg <abdieljrg@gmail.com>
2024-03-08 17:39:25 -0400

    My Commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strstr locates substring
+ *@haystack: pointer one
+ *@needle: pointer two
+ *Return: may vary
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	while (*haystack != '\0')
+	{
+
+		while (*needle != '\0' && *haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+			if (*needle == '\0')
+			{
+				return (haystack);
+			}
+
+			haystack + 1;
+	}
+	return (NULL);
+}
