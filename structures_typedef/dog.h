5c2dc5b3c9d0fc4de080052ac26e84dc7d8e8b3b
Abdieljrg <abdieljrg@gmail.com>
2024-03-20 02:08:59 -0400

    0 1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *struct dog - dog wao wao
+ *@name: name oif the dog
+ *@age: name of the dog
+ *@owner: owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
