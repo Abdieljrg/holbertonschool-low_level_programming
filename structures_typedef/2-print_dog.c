853479d9407d691818dcd812558de1470d38152c
abdieljrg <abdieljrg@gmail.com>
2024-03-20 13:51:22 -0400

    2 0

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *print_field - helps organize dog function
+ *@field: pointer fields inside b
+ *
+ */
+void print_field(char *field)
+{
+	if (field == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%s\n", field);
+	}
+}
+
+/**
+ *print_dog - prints struct dog
+ *@d: pointer of var d
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("name: ");
+	print_field(d->name);
+
+	printf("age: ");
+	if (d->age < 0)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%f\n", d->age);
+	}
+
+	printf("owner: ");
+	print_field(d->owner);
+}
