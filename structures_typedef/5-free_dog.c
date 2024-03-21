357905bd7852a3cd7585d15024e0b69e21940591
abdieljrg <abdieljrg@gmail.com>
2024-03-21 12:24:01 -0400

    5 1

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,16 @@
 #include "dog.h"
 /**
- *free_dog
+ *free_dog - function that frees strcture allocs
+ *@d: pointer in use
  *
- *
- *
+ *Return: void
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	free(d->owner);
 	free(d->name);
 	free(d);
