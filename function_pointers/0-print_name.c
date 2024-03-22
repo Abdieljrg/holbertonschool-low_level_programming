3d6cdd415c12d101404c6367d06e8a63d075e67e
abdieljrg <abdieljrg@gmail.com>
2024-03-22 12:54:19 -0400

    0 new

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,19 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ *print_name - prints pointer name
+ *
+ *@name: pointer of name
+ *@f: function
+ *
+ *Return: if null, does not return anything, if not, name pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	{
+	if (name == NULL || f == NULL)
+		return;
+	}
+	f(name);
+}
